2b12c98a4d7926de7febf222fd0dd4fb4c8ffa69
Nitesh kr sah <sahnitesh1993@gmail.com>
2026-08-08 00:29:39 +0530

    Time: 44 ms (15.97%), Space: 14.2 MB (64.22%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+         for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+            if(nums[i]+nums[j]==target){
+
+                // cout<<"indices are "<<i <<" "<< j<<endl;
+                return{i,j};
+            }
+        }
+    }
+    //  cout<<"no pair found";
+    return {};
+        
+    }
+};
