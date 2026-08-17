360f337866518652925a16440cef742abe584faf
Nitesh kr sah <sahnitesh1993@gmail.com>
2026-08-17 16:23:20 +0530

    Time: 0 ms (100%), Space: 10.3 MB (97.82%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,58 +1,43 @@
 class Solution {
 public:
 
-    bool isPossible(vector<int>& nums, int k, int mid)
-    {
-        int studentCount = 1;
-        int pageSum = 0;
-
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(pageSum + nums[i] <= mid)
-            {
-                pageSum += nums[i];
-            }
-            else
-            {
-                studentCount++;
-                pageSum = nums[i];
-
-                if(studentCount > k)
-                    return false;
-            }
-        }
-
-        return true;
-    }
-
+   
     int splitArray(vector<int>& nums, int k)
     {
-        int low = nums[0];
-        int high = 0;
+       int low=0;
+       int high=0;
+       for(int i=0;i<nums.size();i++){
+        low=max(low,nums[i]);
+        high=high+nums[i];
 
-        for(int i = 0; i < nums.size(); i++)
-        {
-            low = max(low, nums[i]);
-            high += nums[i];
+       }
+
+       int ans=high;
+
+       while(low<=high){
+        int mid=low+(high-low)/2;
+        int count=1;
+        int sum=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(sum+nums[i]<=mid){
+                sum=sum+nums[i];
+            }
+            else{
+                count++;
+                sum=nums[i];
+            }
+        }
+        if(count<=k){
+            ans=mid;
+            high=mid-1;
         }
 
-        int ans = high;
-
-        while(low <= high)
-        {
-            int mid = low + (high - low) / 2;
-
-            if(isPossible(nums, k, mid))
-            {
-                ans = mid;
-                high = mid - 1;
-            }
-            else
-            {
-                low = mid + 1;
-            }
+        else{
+            low=mid+1;
+        
         }
-
-        return ans;
+       }
+       return ans;
     }
 };
