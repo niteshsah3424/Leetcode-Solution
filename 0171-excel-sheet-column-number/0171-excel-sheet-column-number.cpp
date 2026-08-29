de0c64116199944f9a0ab4d0c7ac1c9057623b9d
Nitesh kr sah <sahnitesh1993@gmail.com>
2026-08-29 14:24:09 +0530

    Time: 0 ms (100%), Space: 8.7 MB (73.19%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int ans=0;
+        for(char ch:s){
+            ans=ans*26+(ch-'A'+1);
+        }
+
+      return ans;
+         
+    }
+};
