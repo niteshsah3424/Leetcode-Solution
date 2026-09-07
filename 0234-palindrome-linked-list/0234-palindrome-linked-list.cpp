ce7e3f77f86b663e35e3e7f42a97ec2b80c22b8e
Nitesh kr sah <sahnitesh1993@gmail.com>
2026-09-08 00:15:28 +0530

    Time: 72 ms (5.12%), Space: 152.6 MB (5.03%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,25 +11,35 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        stack<int>st;
-        ListNode*temp=head;
-
-        while(temp!=nullptr){
-            st.push(temp->val);
-            temp=temp->next;
-        }
-
-        ListNode*temp1=head;
-        while(temp1!=nullptr){
-            if(st.top()!=temp1->val){
-                return false;
-                
-            }
-            temp1=temp1->next;
-            st.pop();
-            
-        }
-        return true;
+   ListNode* newnode = NULL;
+   ListNode* temp = head;
+ 
+while (temp != NULL)
+{
+    int value = temp->val;
+ 
+    ListNode* temp1 =new ListNode(value);
+    temp1->next = newnode;
+    newnode = temp1;
+ 
+    temp = temp->next;
+}
+ 
+ListNode* ptr = head;
+ 
+while (ptr != NULL)
+{
+    if (ptr->val != newnode->val)
+    {
+        return false;
+    }
+ 
+    newnode = newnode->next;
+    ptr = ptr->next;
+}
+ 
+return true;
+       
 
     }
 };
